2092df5ab18716dae6b1b498c106d787eff0ac6c
Wellington Lopes <wellington.materiais@gmail.com>
2026-07-14 21:44:32 -0300

    Add dist for GitHub Pages deploy

---
+++ dist/_astro/swimlanesDiagram-G3AALYLV.DZFHxwwh.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./src.BhUfRAbH.js";import"./chunk-WYO6CB5R.Cgs26xK-.js";import"./chunk-ICXQ74PX.BbSbPKA6.js";import"./chunk-HOUHSVGY.Lir3YM6O.js";import"./chunk-Q4XR5HBZ.B6GfPYzC.js";import"./chunk-7BUUIJ7U.CAhmNhj7.js";import"./chunk-OGEWGWER.BhK3mMtM.js";import"./chunk-32BRIVSS.D2dMNsNL.js";import"./chunk-XXDRQBXY.CKP_UVzU.js";import"./chunk-VR4S4FIN.B5hEZ18Z.js";import"./chunk-C7G6YPKG.CIr5tSrF.js";import"./chunk-ZGVPDNZ5.Cs-TptRa.js";import"./chunk-52WLFC77.CPeRdCRu.js";import"./chunk-FWX5IMBZ.3CHyVUil.js";import"./chunk-ZIRB5QZD.Dh20_f-h.js";import{r as t,t as n}from"./chunk-PUDLZKDR.eVCPQyrg.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
